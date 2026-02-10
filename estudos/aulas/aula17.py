de1b23d21a3c3f3770c7b143273a24a80b6175fa
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2026-02-10 14:11:54 +0000

    update 18

--- estudos/aulas/aula17.py
+++ estudos/aulas/aula17.py
@@ -2,30 +2,49 @@ class CapacidadeExcedidaError(Exception):
     pass
 class HorarioInvalidoError(Exception):
     pass
-class SalaIndisponivelErroe(Exception):
+class SalaIndisponivelError(Exception):
     pass
 
 class Sala:
-    def __init__(self,nome,hora_abertura,hora_fechamento,reservas):
+    def __init__(self,nome,hora_abertura,hora_fechamento,capacidade):
         self.nome = nome
         self.hora_abertura = hora_abertura
         self.hora_fechamento = hora_fechamento
-        self.reservas = reservas
-        self.capacidade = 10
+        self.capacidade = capacidade
 
-    def pode_reservar(self,inicio,fim):
-        if not (self.hora_abertura <= inicio <fim <= self.hora_fechamento):
+    def pode_reservar(self,inicio,fim, pessoas):
+        if not (self.hora_abertura <= inicio < fim <= self.hora_fechamento):
             raise HorarioInvalidoError("Horario fora do funcionamento da sala")
-        else :
-            print("Parabens voce foi selecionado")
-        if (self.capacidade < self.reservas):
+        if (pessoas > self.capacidade):
             raise CapacidadeExcedidaError("Capacidade cheia")
-        else:
-            print("Parabens voce foi selecionado")
-
         
-Hospital =Sala("Casa Limpeza", 8, 19,12)
+        return True
 
-Hospital.pode_reservar(18,19)
+class Reserva:
+    reservas  = []
+    def __init__(self,responsavel,inicio,fim,pessoas):
+        self.responsavel = responsavel
+        self.inicio = inicio
+        self.fim = fim
+        self.pessoas = pessoas
+    
+    def validar_horario(self,horario_inicio, horario_termino, pessoas):
+        if  self.inicio <= horario_inicio < horario_termino <= self.fim and  self.pessoas <= pessoas  and Sala.capacidade <= pessoas:
+            return print("A sua reserva pode ser feita")
+        else:
+            raise HorarioInvalidoError("horario e/ou capacidade invalido")
+        
+
+    
+        
+hospital = Sala("Casa Limpeza", 8, 19, 10,)
+ReservaDaniel = Reserva("Carlos", 8,19,9)  
+try:
+    hospital.pode_reservar(17, 18, 5)
+    print("Reserva permitida")
+except Exception as e:
+    print(e)
+
+ReservaDaniel.validar_horario(9,10,8)
 
         
